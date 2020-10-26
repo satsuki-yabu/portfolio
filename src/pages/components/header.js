1b7fa0a4f2c28250fff96e45491e4765bfc9f14b
satsuki-yabu <tetecoo.1019@gmail.com>
2020-10-26 20:04:46 +0900

    フォルダ作成

---
+++ src/pages/components/header.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Header() {
+  return 
+  <h1>Hello Gyatsyby</h1>
+}
--- src/pages/pages/index.js
+++ src/pages/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Header from 'header.js'
+
 
 export default function Home() {
   return <div>Hello world!</div>
